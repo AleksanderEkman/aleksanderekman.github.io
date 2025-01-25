3d496c8670fe01e61880b727926fbb9ee37d799c
allsers <162462688+allsers@users.noreply.github.com>
2025-01-25 15:23:18 +0100

    Vercel speedinsights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,5 @@
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 export const prerender = import.meta.env.DEV || process.env.GITHUB_PAGES === 'true';
+if (process.env.VERCEL === 'true') {
+    injectSpeedInsights();
+}
